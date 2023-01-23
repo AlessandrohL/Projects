7f6d300aee140b7e46b3aa7640a758a9059054f1
Luis Alessandro <luisalexandrocordova@gmail.com>
2023-01-23 15:19:24 -0500

    Replace button

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 import hamburgerMenu from "./js/menu.js";
-import { tooglePlay } from "./js/aud.js";
+
 
 const d = document;
-const $icon = d.querySelector(".fa-play");
-const $audio = d.querySelector('audio');
+
 
 d.addEventListener("DOMContentLoaded", e => {
   hamburgerMenu(".panel-btn", ".panel", ".menu a");
-  tooglePlay(".music-btn", $audio, $icon);
-
 })
 
